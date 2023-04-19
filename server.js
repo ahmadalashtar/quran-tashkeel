fb8c83a655257f254e70e549a3371ed74291f526
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-04-19 09:26:11 +0300

    Add neccessary HTML Get verse Add Style and Script

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
+const request = require('request');
+
 const express = require("express"),
   app = express();
 
 //setting view engine to ejs
 app.set("view engine", "ejs");
 
+const simplify = function(verse){
+  return verse.replace(/[ًٌٍَُِّْ]/g, '');;
+}
+
 //route for index page
 app.get("/", function (req, res) {
   res.render("index");
 });
 
-//route for magic page
-app.get("/magic", function (req, res) {
-  res.render("magic");
-});
+app.get("/verse", (req,res)=>{
+  let verse;
+  let simplifiedVerse;
+  let test = request('http://api.alquran.cloud/v1/ayah/1:1/ar', (error, response, body) => {
+      if (response.statusCode == 200) {
+          result = JSON.parse(response.body);
+          verse = result.data.text;
+          simplifiedVerse = simplify(verse)
+          res.render("index", {simplifiedVerse})
+      } else {
+        res.json(error)
+      }
+    }
+  );
+  console.log(typeof test)
+})
 
 app.listen(3000, function () {
   console.log("Server is running on port 3000 ");
